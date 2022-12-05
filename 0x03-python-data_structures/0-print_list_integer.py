e81e524da4dae9f6b1ffd6500179244a1f2c2921
eliekin <kineliekinahwa@gmail.com>
2022-12-05 14:34:57 +0000

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+	print(n)
+
+
+if __name__ == '__main__':
+    my_list = [2, 4, 6, 8]
+    print_list_integer(my_list)
